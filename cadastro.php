e00b83ab95eb6553a4d028210280aee4c8d6e96e
sidneips <51684093+sidneips@users.noreply.github.com>
2021-04-21 16:58:07 -0300

    update form cadastro (html/php)
    adicionada seguranca MD5 e adicionados campos no banco de dados e forms (nas duas linguagens)

--- cadastro.php
+++ cadastro.php
@@ -1,14 +1,20 @@
 <?php
 
-$login = $_POST['login'];
-$senha = $_POST['senha'];
+$nome = $_POST['cadFirstName'];
+$sobrenome = $_POST['cadLastName'];
+$email = $_POST['cadEmail'];
+$senha = md5($_POST['cadSenha']);
 
 // Cria a string de conexão $strcon
 
-$strcon = mysqli_connect('127.0.0.1','root','','locatec') or die('Erro ao conectar ao banco de dados');
+$strcon = mysqli_connect('127.0.0.1','root','','locatec') 
+    or die('Erro ao conectar ao banco de dados');
 $sql = "INSERT INTO usuarios VALUES ";
-$sql .= "('$login', '$senha')"; 
-mysqli_query($strcon,$sql) or die("Erro ao tentar cadastrar registro");
+$sql .= "('$nome', '$sobrenome', '$email', '$senha')"; 
+
+mysqli_query($strcon,$sql) 
+    or die("Erro ao tentar cadastrar registro");
 mysqli_close($strcon);
+
 echo "Cliente cadastrado com sucesso!";
 ?>
