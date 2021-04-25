354df2698fc15ffb6c6dcf6f1f6f0fe6da9280dc
sidneips <51684093+sidneips@users.noreply.github.com>
2021-04-25 10:32:14 -0300

    update login+subscribe PHP+MD5
    MD5 is working inside PHP and DB

--- @BKP-SIDNEI/20210424-login-MD5/login-MD5.php
+++ @BKP-SIDNEI/20210424-login-MD5/login-MD5.php
@@ -8,7 +8,7 @@
 
 <!--Google fonts-->
 <!--    <link rel="preconnect" href="https://fonts.gstatic.com">-->
-<!--    <link href="https://fonts.googleapis.com/css2?family=Pompiere&display=swap" rel="stylesheet">  -->
+    <link href="https://fonts.googleapis.com/css2?family=Pompiere&display=swap" rel="stylesheet"> 
     
 <!-- [Sidnei] SINCRONIZA A PAGINA A CADA 30 segundos -->
 <!--<meta http-equiv="refresh" content="10"; url="cadastro.html"/>-->
@@ -22,17 +22,18 @@
 </head>
 <body>
 
-<div class="global-body-content">
-<div class="body-content-layer-1">
-	
 <div style="margin-top: 100px; text-align: center; ">
-<h1> <!-- font: 30px pompiere -->
+<!--<h1>
+	font: 30px pompiere
+-->
 
 <!-- ---------------------------- INICIO PHP -->
 <?php
 
 $email = $_POST['email'];
-$senha = md5($_POST['senha']); //Transforma a senha recebida em HASH para validar com a HASH salva no BD
+$senha = md5($_POST['senha']); 
+//Habilitando md5 a senha não funciona md5($_POST['senha']);
+
 
 
 //================================= Seguranca contra SQL INJECTION
@@ -42,29 +43,27 @@ $senha = stripcslashes($senha);
 //$senha = mysqli_real_escape_string($senha);
 
 
+
 //================================= Conexao com banco de dados
 $con = mysqli_connect("localhost","root","","locatec"); //IP(hostname) - User - Pass - DB
-
-//------------ PARA FINS DE TESTE E VALIDACAO DA CONEXAO DB -------------- INICIO
 if($con){
 	echo "teste conexao DB - OK <br>";
 }else{
 	die(mysqli_error($con));
 }
-//------------ PARA FINS DE TESTE E VALIDACAO DA CONEXAO DB -------------- FIM
+
+//mysql_select_db("locatec");
+
+
 
 //================================= Conexao com dados do usuario
-	//------------ PARA FINS DE TESTE E VALIDACAO DA QUERY -------------- INICIO
-		echo "antes da query - OK <br>";
-	//------------ PARA FINS DE TESTE E VALIDACAO DA QUERY -------------- FIM
-
+	echo "antes da query - OK <br>";
+	
 $result = mysqli_query($con,"select * from usuarios where email = '$email' and senha = '$senha'") 
-				or die("Erro na query solicitada; 
-					<br>ERROR.MSG: " .mysqli_error($result));
+				or die("Dados incorretos " .mysqli_error($result));
 
-	//------------ PARA FINS DE TESTE E VALIDACAO DA QUERY -------------- INICIO
-		echo "depois da query - OK <br>";
-	//------------ PARA FINS DE TESTE E VALIDACAO DA QUERY -------------- FIM
+	echo "depois da query - OK <br>";
+
 
 //================================= Dados trazidos pela conexao acima
 $row = mysqli_fetch_array($result);
@@ -85,52 +84,51 @@ if (is_null($email) || is_null($senha))
 }
 else
 {
-	// -----------------------------------------
-	if($email == null){ //Testa se o email está em branco
-		die("<br> Usuário não preenchido");
-	
-	}else{
-		// TESTA SE O EMAIL EXISTE NO BANCO
+		// TESTA SE OS DADOS EXISTEM NO BANCO
 		if (isset($row['email']) == $email) //Testa se email digitado existe no DB através da query
-		{	
+		{
 			// -----------------------------------------
-			if($senha == null){//Testa se a senha está em branco
-				die("<br> Senha não preenchida");
+			if($email == null){ //Testa se o email está em branco
+				echo "<br> Usuário não preenchido";
 			}else{
-				
-				if(isset($row['senha']) == $senha) //Testa se a "HASH DA SENHA" digitada existe no DB através da query
-				{
+				// -----------------------------------------
+				if (isset($row['senha']) == $senha){ //Testa se a "HASH DA SENHA" digitada existe no DB através da query
 					die('<br> <b>Conectado com sucesso!</b> 
-					<br> Redirecionando... 
-					<br><span style="color: red;">(pendente de desenvolvimento | auto redir em 5 segundos)</span>
-					<a href="login.html"> -- Retornar</a>'
-					);
+					<br> Redirecionando... <br><span style="color: red;">(pendente de desenvolvimento | auto redir em 5 segundos)</span>');
+					//<a href="login.html"> -- Retornar</a>
+
 					/* set_time_limit(5);
 					header("Location: cadastro.html");// localhost/ */
+					
+					if($senha == null){
+						echo "<br> Senha não preenchida";
+					}
 				}
-			} //ELSE senha em branco - FIM
-		}//IF email existe no banco -FIM
+				// -----------------------------------------
+			}
+			// -----------------------------------------
+		}
 		else
 		{
 			echo "<br> <b>Login ou senha incorretos...</b>"; //.$result;
 			echo "<br> Tente novamente.";
 			
 			//------------ PARA FINS DE TESTE E VALIDACAO DO LOGIN -------------- INICIO
-			
+
 				//echo "<br><br> senha trazida do banco: ".isset($row['senha']);
 				//echo "<br><br> <b>Senha digitada (já embaralhada -MD5) :</b> $senha";
 						//<br><b>Senha+MD5:</b> " .md5($senha);
-			
+
 						//echo "<br> Login digitado: $email";
 						//echo "<br> Senha digitada: $senha";
-			
+
 			//------------ PARA FINS DE TESTE E VALIDACAO DO LOGIN -------------- FIM
-			
+
 			/* set_time_limit(5);
 			header("Location: cadastro.html");// localhost/ */
-			
-		} //ELSE senha INCORRETA - FIM
-	} //ELSE senha em branco - FIM
+
+		} // FIM ELSE 02
+
 } // FIM ELSE 01
 
 
@@ -138,11 +136,11 @@ else
 <!-- ---------------------------- FIM PHP -->
 
 
-</h1>
+<!--</h1>-->
 <br><br>
-   <a href="login.html">Retornar</a>
+   <a href="login-md5.html">Retornar</a>
 <br>
-Retornando a pagina anterior... (pendente de desenvolvimento)
+Retornando a pagina anterior...
 <br>
 
 </div>
