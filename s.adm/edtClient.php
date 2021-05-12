462b1fab7d7ad6e1e2088e3fa9b68e7bf1c42841
sidneips <51684093+sidneips@users.noreply.github.com>
2021-05-12 20:54:53 -0300

    FAV-ICON
    Icone do Website adicionado

--- s.adm/edtClient.php
+++ s.adm/edtClient.php
@@ -5,6 +5,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/main.css">
+<link rel="icon" href="http://localhost/img/favicon/fav5.jpg" type="image/jpg" sizes="32x32">
     
 <!-- [Sidnei] SINCRONIZA A PAGINA A CADA 30 segundos -->
 <!-- <meta http-equiv="refresh" content="5">  -->
